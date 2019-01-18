bd472d543e2a7c85c144b5c5f56e6fc59eff708e
Raja Simon <rajasimon@icloud.com>
2019-01-18 23:38:37 +0530

    Corrected github token key

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ setup_git() {
 push_backend() {
   cd backend
   git init
-  git remote add origin https://${GH_TOKEN}@github.com/rajasimon/testcrm.git > /dev/null 2>&1
+  git remote add origin https://${GITHUB_TOKEN}@github.com/rajasimon/testcrm.git > /dev/null 2>&1
   git add --all
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
   git push --quiet --force --set-upstream origin master
@@ -17,7 +17,7 @@ push_backend() {
 push_frontend() {
   cd frontend
   git init
-  git remote add origin https://${GH_TOKEN}@github.com/rajasimon/testcrm.git > /dev/null 2>&1
+  git remote add origin https://${GITHUB_TOKEN}@github.com/rajasimon/testcrm.git > /dev/null 2>&1
   git add --all
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
   git push --quiet --set-upstream origin master
